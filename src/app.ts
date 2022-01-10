7a79925abd0356343dee4ae90393cff3c4406e08
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2022-01-10 14:25:33 -0300

    First commit

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from '@ytickets/common';
+import { NotFoundError } from '@ytickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    // NODE_ENV is set by jest
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(errorHandler);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+export { app };
